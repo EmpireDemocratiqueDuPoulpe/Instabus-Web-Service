f20394bead5378e517b11320918ccc2e5761689d
Empire Démocratique du Poulpe <alexis.lecomte@supinfo.com>
2021-01-31 21:04:55 +0000

    Fixed user pics script

--- getUserPics.php
+++ getUserPics.php
@@ -5,8 +5,8 @@ $posts = new Post($db);
 $user_id = $_GET["user_id"] ?? null;
 $station_id = $_GET["station_id"] ?? null;
 
-if (!is_null($user_id) && !is_null($station_id)) {
+if (!is_null($user_id) || !is_null($station_id)) {
     echo json_encode($posts->getAll($user_id, $station_id));
 } else {
-    echo json_encode(["error" => "missing user_id or post_id"]);
+    echo json_encode(["err" => "Missing user_id or station_id"]);
 }
